9ceb8ec3c262ea7bf5b275104800a70bc07dcc08
anoop raju <anoopraju31@gmail.com>
2024-09-26 14:35:46 +0530

    Sign Up service added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Post } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signin() {
-    return {
-      message: 'I am signin',
-      status: 200,
-    };
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    // * Generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    // * Save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        password: hash,
+      },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        password: false,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    // * Return the saved user
+    return user;
   }
 
-  signup() {
-    return {
-      message: 'I am signup',
-      status: 200,
-    };
+  signin() {
+    return { message: 'I am sign in' };
   }
 }
